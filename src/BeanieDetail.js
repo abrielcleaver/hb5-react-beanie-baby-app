a5f645199d4e279b2ec016ff28a6bd4abcef2424
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-08 01:19:50 -0700

    Add detail page for beanie babies. beanie baby detail page renders properly with all information

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
   const [beanieBaby, setBeanieBaby] = useState({});
   // you'll need to get the route param using the appropriate react router hook
+  const params = useParams();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
+    async function getBeanieBaby() {
+      const data = await getSingleBeanie(params.id);
 
+      setBeanieBaby(data);
+    }
+    getBeanieBaby();
   }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
 
   function handleBeanieClick() {
@@ -20,7 +26,7 @@ export default function BeanieDetail() {
     <>
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
       
-      {/* <Link to='/'>Home</Link>
+      <Link to='/'>Home</Link>
       <div className='beanie-detail' onClick={handleBeanieClick}>
         <div className='beanie-data'>
           <p>{beanieBaby.animal}</p>
@@ -39,7 +45,7 @@ export default function BeanieDetail() {
           <p>Swing Tag Generation: {beanieBaby.swingTagGeneration}</p>
           <p>Tush Tag Generation: {beanieBaby.tushTagGeneration}</p>
         </div>
-      </div>   */}
+      </div>
     </>
   );
 }
