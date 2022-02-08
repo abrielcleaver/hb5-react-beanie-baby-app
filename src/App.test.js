3dad68fa673c67372e5f7e3c319c9b9d18084a45
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-08 00:57:39 -0700

    Update test to check for current page text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders current page text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const currentPage = screen.getByText(/current page/i);
+  expect(currentPage).toBeInTheDocument();
 });
