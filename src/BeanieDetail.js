ac7d5674bd07637393d8fb2b856051200271c5e8
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-08 00:43:17 -0700

    Track state for current beanieBaby

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -4,6 +4,7 @@ import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
+  const [beanieBaby, setBeanieBaby] = useState({});
   // you'll need to get the route param using the appropriate react router hook
 
   useEffect(() => {
