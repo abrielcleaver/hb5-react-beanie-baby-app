548ff3e89f6ec7b160b25286d5dfe3c96b9b93d7
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-08 00:47:17 -0700

    Render out beanie babies as list

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,6 +4,12 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    // null
+    <Link to={`/beanie-babies/${beanieBaby.id}`}>
+      <div className='baby'>
+        <p>{beanieBaby.title}</p>
+        <img className='beanie-img' src={beanieBaby.image} />
+      </div>
+    </Link>
   );
 }
